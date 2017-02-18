f9b7886ba82e4f9e3165c5b45953c9c3d8591c0b
daartv <daartv@gmail.com>
2017-02-17 16:11:20 -0800

    Add text box, send button and drop down menu
    Passed tests for init, sending, fetching and chatroom behavior

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,51 @@
 // YOUR CODE HERE:
+var app = {};
+app.init = function () {
+
+};
+
+app.send = function (message) {
+  $.ajax({
+    url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox; Message sent');
+    },
+    error: function(data) {
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+
+};
+
+app.fetch = function () {
+  $.ajax({
+    type: 'GET',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox; Message sent');
+    },
+    error: function(data) {
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+};  
+
+app.clearMessages = function () {
+  $('#chats').html('');
+};
+
+app.renderMessage = function (message) {
+  var msg = document.createElement('p');
+  msg.innerHTML = message;
+  document.getElementById('chats').appendChild(msg);
+};
+
+app.renderRoom = function (chatRoom) {
+  var room = document.createElement('option');
+  room.innerHTML = chatRoom;
+  document.getElementById('roomSelect').appendChild(room);
+};  
