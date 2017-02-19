a9b0eb0079ddf3d038b3b4eaf256d759d75dcc82
daartv <daartv@gmail.com>
2017-02-18 17:33:19 -0800

    End of day commit

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -13,9 +13,17 @@ $(document).ready(function () {
   });
   app.handleSubmit();
   app.fetch();
+
+
+  $('#thisuser').on('click', function () {
+    var clickedBtnID = $(this).attr('id'); // or var clickedBtnID = this.id
+    console.log('you clicked on button #' + clickedBtnID);
+  });
+
+
 });
 
-window.userStorage = [];
+var messages = [];
 
 window.friends = {};
 
@@ -51,20 +59,19 @@ app.fetch = function () {
     contentType: 'application/json',
     // dataType: 'json',
     success: function (data) {
-      console.log(data);
-      // console.log('chatterbox; Message fetch');
-      for (var i = data.results.length - 1; i >= 0; i--) {     
-        app.renderMessage(data.results[i]);
-        app.renderRoom(data.results[i]);
-      }
+      app.messages = data.results;
+      var mostRecentMessage = app.messages[app.messages.length - 1];
+      if (mostRecentMessage.objectId !== app.lastMessageId) {
+        app.renderMessage(app.messages);
+        app.renderRoomList(app.messages);
+      }     
     },
     error: function(data) {
       console.error('chatterbox: This is a \'failed to send\' message from app.fetch');
     }
   });
 };  
-
-
+//console.log(window.chatRooms);
 app.clearMessages = function () {
   $('#chats').html('');
   $('#message').innerHTML = '';
@@ -75,36 +82,50 @@ app.renderMessage = function (obj) {
   // var atag = document.createElement('a');
   // // atag.innerHTML = obj.username;
   // msg.innerHTML = obj.text;
-  var htmlEscapes = {
-    '&': '&amp;',
-    '<': '&lt;',
-    '>': '&gt;',
-    '"': '&quot;',
-    "'": '&#x27;',
-    '/': '&#x2F;'
-  };
-  var htmlEscaper = /[&<>"'\/]/g;
-  _.escape = function(string) {
-    return ('' + string).replace(htmlEscaper, function(match) {
-      return htmlEscapes[match];
-    });
-  };
   $('#chats').prepend('<p>' + _.escape(obj.text) + '</p>');
-  $('#chats').prepend('<a href = "#" onclick="app.handleUsernameClick()">' + _.escape(obj.username) + '</a>');
+  $('#chats').prepend('<a href = "#" onclick="app.handleUsernameClick()" id="thisuser">' + _.escape(obj.username) + '</a>');
+
 };
 
+var htmlEscapes = {
+  '&': '&amp;',
+  '<': '&lt;',
+  '>': '&gt;',
+  '"': '&quot;',
+  "'": '&#x27;',
+  '/': '&#x2F;'
+};
+var htmlEscaper = /[&<>"'\/]/g;
+_.escape = function(string) {
+  return ('' + string).replace(htmlEscaper, function(match) {
+    return htmlEscapes[match];
+  });
+};
   
 
-app.renderRoom = function (chatRoom) {
-  $('#roomSelect').append( chatRoom.roomname);
- // room.innerHTML = chatRoom;
-  //document.getElementById('roomSelect').appendChild(room);
-};  
+app.renderRoomList = function (data) {
 
-app.handleUsernameClick = function (data) {
-  userStorage.push($('a'));
-  console.log('user clicked!');
-  //console.log(data.username);
+  $('#roomSelect').html('<option value="__newRoom">New room...</option></select>');
+  if (data) {
+    var rooms = {};
+    data.forEach(function(message) {
+      var roomname = message.roomname;
+      if (roomname && !rooms[roomname]) {
+        app.renderRoom(roomname);
+      }
+    });
+  }
+};
+
+app.renderRoom = function (roomname) {
+  var $option = $('<option/>').val(roomname).text(roomname);
+  $('#roomSelect').append($option);
+};
+
+app.handleUsernameClick = function () {
+
+  //console.log($('#thisuser'));
+  
   
 };
 
@@ -121,18 +142,17 @@ app.getUserName = function () {
 };
 
 var userName = app.getUserName();
-//console.log(app.getUserName());
+//console.log(userName)
 
-app.handleSubmit = function (value) {
+app.handleSubmit = function (value) { 
+  var input = {
+    username: app.userName,
+    text: $('#message').val(),
+    roomname: app.roomname || 'lobby'
+  };
+
   //var text = getElementById('message').innerHTML;
   $('#send').click(function() {
-    var text = $('#message').val();
-    var input = {
-      username: userName,
-      text: text,
-      //roomname: roomname
-    };
-
     if (input.text !== '') {  
       app.renderMessage(input);
       app.send(input);
@@ -140,11 +160,4 @@ app.handleSubmit = function (value) {
     
     document.getElementById('message').value = '';
   });
-
 };
-
-
-
-// app.handleUsernameClick.on('click', function () {
-//   return true;
-// });
